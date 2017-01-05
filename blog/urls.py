7a1ba3974bd197c1851a46de924cf90f5258212e
WeirdHippo <nunjiho@naver.com>
2017-01-05 19:37:01 +0900

    django 비동기, 동기 youtube api

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls import url
+from . import  views
+
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^search$',views.search_api, name='search_api'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,43 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from apiclient.discovery import build
 
 # Create your views here.
+
+DEVELOPER_KEY = "AIzaSyBSBMkFC_fyyGs1cVqubH26sb4OkkFyuD4"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+def search_name(keyword):
+
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+    search_response = youtube.search().list(
+        q=keyword,
+        part="id,snippet",
+        maxResults=25
+    ).execute()
+
+    return search_response
+
+
+def search_word(keyword):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+
+
+    search_rep = youtube.search().list(
+    q=keyword,
+    part="id,snippet",
+        maxResults=25
+    ).execute()
+
+    return search_rep
+
+def search_api(request):
+    a = request.POST['keyword']
+    print(a)
+    # request.post
+    #search_name(a)
+    return JsonResponse({ "word" : search_word(a) , "name" : search_name(a)})
 
 def post_list(request):
     return render(request, 'blog/index.html', {})
